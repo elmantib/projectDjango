bf348e660c707c999c4b08ab65f859ab289a665a
elmantib <elmantibrahim3@gmail.com>
2023-12-10 04:19:31 -0500

    first commit

---
+++ firstapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path object defining the URL pattern to the index view
+    path(route='', view=views.index, name='index'),
+    # another path object defining the URL pattern using `/date` prefix
+     path(route='date', view=views.get_date, name='date'),
+]
+
